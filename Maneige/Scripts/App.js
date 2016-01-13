311cac8d5dacde4c819cb8fac69991acd0301c2e
Victor <roxolanus@gmail.com>
2016-01-13 20:17:50 +0200

    Redoing after crash caused by npm introduction

--- Maneige/Scripts/App.js
+++ Maneige/Scripts/App.js
@@ -21,7 +21,7 @@ function initializePage()
     // This function is executed if the above call is successful
     // It replaces the contents of the 'message' element with the user name
     function onGetUserNameSuccess() {
-        $('#message').text('Hello ' + user.get_title());
+        $('#message').text('Вітаємо, ' + user.get_title());
     }
 
     // This function is executed if the above call fails
